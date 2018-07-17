5b69e85ce52df4edd29c5dff9cad80028234fe0e
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-07-16 20:25:17 -0500

    Log In, Designs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      @class_index = 'btn btn-outline-dark my-2 my-sm-0 invisible'
+    else
+      @class_index = 'btn btn-outline-dark my-2 my-sm-0'
+    end
   end
 end
