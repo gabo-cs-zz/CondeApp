ae46c773179f803e72825c8ba129986cc88b3483
Gabriel Coronado <gabrielomar2809@gmail.com>
2018-07-24 19:44:03 -0500

    Assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require popper
+//= require turbolinks
 //= require bootstrap
 //= require jquery
 //= require jquery_ujs
-//= require popper
-//= require turbolinks
 //= require bootstrap-sprockets
 $(document).ready(function(){
     $(".dropdown-toggle").dropdown();
